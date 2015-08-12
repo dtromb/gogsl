63646a1e37952de3e7ead22f5a0d2b4c6e1a501b
Dave Trombley <dave.trombley@gmail.com>
2015-08-12 17:16:02 -0400

    removed old 'tuple' directory

--- tuple/tuple.go
+++
@@ -1,8 +0,0 @@
-//////////        AUTOMATICALLY GENERATED CODE - DO NOT EDIT        //////////
-package tuple
-
-/*
-   #cgo pkg-config: --define-variable=prefix=. gsl
-   #include <gsl/gsl_tuple.h>
-*/
-import "C"
