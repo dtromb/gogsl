c6530805bd7a5be73abe8471780d746954932c39
Dave Trombley <dave.trombley@gmail.com>
2015-08-18 00:41:50 -0400

    additional root test, fixed 5.0/0.5 sqrt arg

--- root/test/root_test.go
+++ root/test/root_test.go
@@ -38,7 +38,7 @@ func TestRoot(t *testing.T) {
 	qp := &QuadraticParams{
 		A: 1.0,
 		B: 0.0,
-		C: -0.5,
+		C: -5.0,
 	}
 
 	f := &gogsl.GslFunction{
@@ -67,6 +67,50 @@ func TestRoot(t *testing.T) {
 		}
 		fmt.Printf("%5d [%.7f, %.7f] %.7f %+.7f %.7f\n",
 			iter, xLo, xHi, r, r-rExpected, xHi-xLo)
+		if status != gogsl.GSL_CONTINUE || iter >= maxIter {
+			break
+		}
+	}
+}
+
+func TestRootFdf(t *testing.T) {
+
+	var maxIter int = 100
+	var x float64 = 5.0
+	var rExpected float64 = math.Sqrt(5.0)
+
+	qp := &QuadraticParams{
+		A: 1.0,
+		B: 0.0,
+		C: -5.0,
+	}
+
+	f := &gogsl.GslFunctionFdf{
+		Function:   Quadratic,
+		Derivative: QuadraticDeriv,
+		Fdf:        QuadraticFdf,
+		Params:     qp,
+	}
+	gogsl.InitializeGslFunctionFdf(f)
+
+	T := root.GSL_ROOT_FDFSOLVER_NEWTON
+	s := root.FdfsolverAlloc(T)
+	root.FdfsolverSet(s, f, x)
+
+	fmt.Printf("using %s method\n", root.FdfsolverName(s))
+	fmt.Printf("%-5s %10s %10s %10s\n", "iter", "root", "err", "err(est)")
+
+	iter := 0
+	for {
+		iter += 1
+		root.FdfsolverIterate(s)
+		x0 := x
+		x = root.FdfsolverRoot(s)
+		status := gogsl.GslError(root.TestDelta(x, x0, 0, 1e-3))
+		if status == gogsl.GSL_SUCCESS {
+			fmt.Printf("Converged:\n")
+		}
+		fmt.Printf("%5d %10.7f %+10.7f %10.7f\n", iter, x, x-rExpected, x-x0)
 		if status != gogsl.GSL_CONTINUE || iter >= maxIter {
 			break
 		}
